83c5c896f810365a2d7b1da30d9a87c280beed38
днс <kreshvan12317@yandex.ru>
2024-05-05 22:47:25 +0300

    исправленная дз

---
+++ src/Main.java
@@ -0,0 +1,93 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+                //задача 1
+                var dog = 0.8;
+                System.out.println(dog);
+                var cat = 3.6;
+                System.out.println(cat);
+                var paper = 763789;
+                System.out.println(paper);
+
+                //задача 2
+                dog = 0.8 + 2;
+                System.out.println(dog);
+                cat = 3.6 - 1.6;
+                System.out.println(cat);
+                paper = 763789 + 2;
+                System.out.println(paper);
+
+
+
+                //задача 3
+                var dog1 = dog - 3.5;
+                System.out.println(dog1);
+                var cat1 = cat - 1.6;
+                System.out.println(cat1);
+                var paper1 = paper - 7639;
+                System.out.println(paper1);
+
+                //задача 4
+
+                var friend = 19;
+                System.out.println(friend);
+                friend = friend + 2;
+                System.out.println(friend);
+                friend = friend / 7;
+                System.out.println(friend);
+
+                //задача 4
+
+                var frog = 3.5;
+                System.out.println(frog);
+                frog = frog * 10;
+                System.out.println(frog);
+                frog = frog / 3.5;
+                System.out.println(frog);
+                frog = frog + 4;
+                System.out.println(frog);
+
+                //задача 5
+
+                var sportsmen1 = 78.2;
+                var sportsmen2 = 82.7;
+                System.out.println(sportsmen1 + sportsmen2);
+                System.out.println(sportsmen2 - sportsmen1);
+
+                //задача 7
+                var sportsmen1r = 78.2;
+                var sportsmen2r = 82.7;
+
+                System.out.println(sportsmen2r % sportsmen1r);
+
+                // задача 8
+
+                var hours = 640;
+                var peopleHours = 8;
+                var sumPeople = hours / peopleHours;
+                System.out.println("Всего работников в компании — " + sumPeople);
+
+                //задача 8.1
+
+                var dopPeople = 94;
+                var sumPeople1 = dopPeople + sumPeople;
+                var hours1 = hours / sumPeople1;
+                System.out.println("Если в компании работает" + " " + sumPeople1 + " " + " " + "человек, то всего" + " " + hours1 + " " + "часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+
